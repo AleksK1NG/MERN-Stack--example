b54bca0433c8b42f26e0d4a329f192bcf161e226
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-09 13:46:18 +0300

    add simple Item model

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const Item = require('./models/Item');
+
 const app = express();
 
 /*
