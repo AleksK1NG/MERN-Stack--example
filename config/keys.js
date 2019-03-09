a6d19e29dd4e3d5a8d54b3283d80aafc1e43cda2
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-09 13:40:26 +0300

    init simple server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://alex:alexpro777@ds243418.mlab.com:43418/mern-example'
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+/*
+ * BodyParser middleware
+ * */
+app.use(bodyParser.json());
+
+/*
+ * DB Mlab config
+ * */
+const db = require('./config/keys').mongoURI;
+
+/*
+ * Connect to MongoDB
+ * */
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+/*
+ *   Run server
+ * */
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
