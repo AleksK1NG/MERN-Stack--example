a0b83a7f3afd0f4e31fd2d85a57ec1ff0297517d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-11 18:25:27 +0300

    add register user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 const User = require('../../models/User');
 
@@ -7,9 +8,55 @@ const User = require('../../models/User');
 // @Register new user
 // @access Public
 router.post('/', (req, res) => {
+  const { name, email, password } = req.body;
 
-  res.status(200).send('Success')
-})
+  // Simple validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Please enter all fields' });
+  }
 
+  // Check for existing model
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user) return res.status(400).json({ message: 'User already exists' });
+
+      const newUser = new User({
+        name,
+        email,
+        password
+      });
+
+      // Create salt and hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => {
+              res.status(200).json({
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json({
+                error: err
+              });
+            });
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
 module.exports = router;
